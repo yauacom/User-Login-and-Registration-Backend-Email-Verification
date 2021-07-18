ef1a0fcd6f8a61565416f669f6505a7943dd1baa
yauacom <70412615+yauacom@users.noreply.github.com>
2021-07-18 16:15:24 -0700

    initialize project

---
+++ src/main/java/com/sang/userLoginAndRegistration/UserLoginAndRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.sang.userLoginAndRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginAndRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginAndRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sang/userLoginAndRegistration/UserLoginAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sang.userLoginAndRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginAndRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
