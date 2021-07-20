0984bec1303731f47f298ae80ca1b25994e380b5
yauacom <70412615+yauacom@users.noreply.github.com>
2021-07-19 23:46:30 -0700

    Fixed errors and added spring security config

--- src/main/java/com/sang/userLoginAndRegistration/registration/RegistrationService.java
+++ src/main/java/com/sang/userLoginAndRegistration/registration/RegistrationService.java
@@ -1,8 +1,12 @@
 package com.sang.userLoginAndRegistration.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
 
   public String register(RegistrationRequest request) {
+
     return "works";
   }
 }
