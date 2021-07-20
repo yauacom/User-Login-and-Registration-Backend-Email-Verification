36c3cda6f3d88639c7aae96a0b15b1037dfdbb22
yauacom <70412615+yauacom@users.noreply.github.com>
2021-07-20 00:50:10 -0700

    added email validator and registration service

--- src/main/java/com/sang/userLoginAndRegistration/appuser/AppUserService.java
+++ src/main/java/com/sang/userLoginAndRegistration/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,27 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
   private final AppUserRepository appUserRepository;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     return appUserRepository.findByEmail(email)
         .orElseThrow(() -> new UsernameNotFoundException(String.format("user with email %s not found", email)));
+  }
+
+  public String signUpUser(AppUser appUser) {
+    boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+    if(userExists) {
+      throw new IllegalStateException(String.format("Email %s was already taken", appUserRepository.findByEmail(
+          appUser.getEmail())));
+    }
+    String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+    appUser.setPassword(encodedPassword);
+
+    appUserRepository.save(appUser);
+
+//    TODO: Send confirmation token
+
+    return "it works";
   }
 }
